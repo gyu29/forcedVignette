5eb1ee549074841a3005a8adf02166af3f3f747b
Gene Yu <112839874+gyu29@users.noreply.github.com>
2024-05-27 00:59:13 +0900

    Add files via upload

---
+++ src/main/java/com/forcedVignette/gyu29/forcedVignette.java
@@ -0,0 +1,28 @@
+package com.forcedVignette.gyu29;
+
+import net.weavemc.loader.api.ModInitializer;
+import net.weavemc.loader.api.command.CommandBus;
+import net.weavemc.loader.api.event.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+import org.lwjgl.input.Keyboard;
+
+public class forcedVignette implements ModInitializer {
+    @Override
+    public void preInit() {
+        System.out.println("Initializing ExampleMod!");
+
+        CommandBus.register(new TestCommand());
+
+        EventBus.subscribe(KeyboardEvent.class, e -> {
+            if (Minecraft.getMinecraft().currentScreen == null && e.getKeyState()) {
+                Minecraft.getMinecraft().thePlayer.addChatMessage(
+                        new ChatComponentText("Key Pressed: " + Keyboard.getKeyName(e.getKeyCode()))
+                );
+            }
+        });
+        EventBus.subscribe(RenderHandEvent.class, e -> e.setCancelled(true));
+
+        EventBus.subscribe(new RenderGameOverlayListener());
+    }
+}
